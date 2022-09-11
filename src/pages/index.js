d9daae2240bef07b8d9e226db3e296ec331f4160
AJ <ancheetah@users.noreply.github.com>
2022-09-10 23:45:55 -0400

    mobile skeleton

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,20 @@
 import * as React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, 
+  Row, 
+  Column, 
+  Card,
+  Button,
+  Navbar,
+  Form,
+  InputGroup,
+ } 
+ from "react-bootstrap"
 
 const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  backgroundColor: "#F6F8FF",
+  // padding: 96,
+  // fontFamily: "-apple-system, Roboto, sans-serif, serif",
 }
 const headingStyles = {
   marginTop: 0,
@@ -126,47 +137,53 @@ const links = [
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+
+    <header>
+      <Navbar>
+        <Container>
+          <Navbar.Brand>devfinder</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              DARK
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+      
+    <Container>
+      <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <InputGroup>
+          <InputGroup.Text>O</InputGroup.Text>
+          <Form.Control type="text" placeholder="Search Github username..." />
+          <Button>Search</Button>
+        </InputGroup>
+      </Form.Group>
+      </Form>
+    </Container>
+
+    <Container>
+      <Card>
+        <Card.Body>
+          <Card.Title>The Octocat</Card.Title>
+          <a href="#">@octocat</a>
+          <Card.Subtitle className="mb-2 text-muted">Joined 25 Jan 2011</Card.Subtitle>
+          <Card.Text>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros.
+          </Card.Text>
+          <ul>
+            <li><Card.Link href="#">San Francisco</Card.Link></li>
+            <li><Card.Link href="#">www.example.com</Card.Link></li>
+            <li><Card.Link href="#">Twitter</Card.Link></li>
+            <li><Card.Link href="#">@github</Card.Link></li>
+          </ul>
+      
+        </Card.Body>
+      </Card>
+    </Container>
+
     </main>
   )
 }
